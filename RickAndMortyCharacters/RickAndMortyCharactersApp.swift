095c37132ea2bae2d78ab4d6dce51fcbe936077a
Mostafa Alaa <mostafaassassin@gmail.com>
2024-07-04 23:47:56 +0300

    Initial Commit

---
+++ RickAndMortyCharacters/RickAndMortyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCharactersApp.swift
+//  RickAndMortyCharacters
+//
+//  Created by Mostafa Alaa on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
