31557c4e7b9907d7bc97bedaee04f79a41bdc2da
Mostafa Alaa <mostafaassassin@gmail.com>
2024-07-06 04:12:32 +0300

    created basic generic network manager (#2)

---
+++ RickAndMortyCharacters/Utilities/Network/Endpoints.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoints.swift
+//  RickAndMortyCharacters
+//
+//  Created by Mostafa Alaa on 06/07/2024.
+//
+
+import Foundation
+
+struct Endpoints {
+    static let baseURL = "https://rickandmortyapi.com/api/"
+
+}
