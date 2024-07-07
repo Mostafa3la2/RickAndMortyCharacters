8a5f3ad7608acd32e33fd35daf4f7a308e67f8f3
Mostafa Alaa <mostafaassassin@gmail.com>
2024-07-08 02:46:38 +0300

    added integration testing, enhanced the network manager
    while working on the integration testing, i enhanced the network manager by providing better error handling

--- RickAndMortyCharacters/Utilities/Network/NetworkManager.swift
+++ RickAndMortyCharacters/Utilities/Network/NetworkManager.swift
@@ -9,12 +9,12 @@ import Foundation
 import Combine
 
 protocol NetworkManager {
-    func fetchAllCharacters(withParameters parameters: [String: String]) -> AnyPublisher<CharactersPageDTO, Error>
+    func fetchAllCharacters(withParameters parameters: [String: String]) -> AnyPublisher<(data: CharactersPageDTO, response: URLResponse), Error>
 }
 class DefaultNetworkManager: NetworkManager {
     private let apiClient = APIClient()
 
-    func fetchAllCharacters(withParameters parameters: [String: String]) -> AnyPublisher<CharactersPageDTO, Error> {
+    func fetchAllCharacters(withParameters parameters: [String: String]) -> AnyPublisher<(data: CharactersPageDTO, response: URLResponse), Error> {
         guard let url = Endpoints.characters(with: parameters) else {
             return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
         }
--- RickAndMortyCharactersTests/CharactersListViewModelTests.swift
+++ RickAndMortyCharactersTests/CharactersListViewModelTests.swift
@@ -13,24 +13,30 @@ final class CharactersListViewModelTests: XCTestCase {
 
     private var viewModel: CharactersListViewModel!
     private var mockNetworkManager: MockNetworkManager!
+    private var networkManager: DefaultNetworkManager!
     private var dataSource: CharactersRemoteDataSource!
     private var repository: CharacterRepository!
     private var useCase: FetchAllCharactersUseCase!
+    private var cancellables: Set<AnyCancellable>!
 
     override func setUp() {
         super.setUp()
         mockNetworkManager = TestDIContainer.shared.makeMockNetworkManager()
+        networkManager = DefaultNetworkManager()
         dataSource = TestDIContainer.shared.makeDefaultCharacterDataSource(networkManager: mockNetworkManager)
         repository = TestDIContainer.shared.makeCharacterRepository(dataSource: dataSource)
         useCase = TestDIContainer.shared.makeFetchAllCharactersUseCase(repo: repository)
         viewModel = TestDIContainer.shared.makeCharacterListViewModel(usecase: useCase)
+        cancellables = []
     }
     override func tearDown() {
         viewModel = nil
         dataSource = nil
+        networkManager = nil
         useCase = nil
         repository = nil
         mockNetworkManager = nil
+        cancellables = nil
         super.tearDown()
     }
 
@@ -102,7 +108,7 @@ final class CharactersListViewModelTests: XCTestCase {
                                            episode: nil)
                                    ]
         let apiResponseDTO = CharactersPageDTO(info: InfoDTO(count: 4, pages: 4, next: nil, prev: nil), characters: apiCharactersDTO)
-        mockNetworkManager.fetchCharactersResult = Just(apiResponseDTO)
+        mockNetworkManager.fetchCharactersResult = Just((apiResponseDTO, URLResponse()))
             .setFailureType(to: Error.self)
             .eraseToAnyPublisher()
         let expectation = expectation(description: "Characters fetched and mapped successfully")
@@ -114,5 +120,27 @@ final class CharactersListViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.charactersListItems.first?.name, expectedCharacters.first?.name)
 
         self.wait(for: [expectation], timeout: 1)
+    }
+
+    func testAPIIntegration() {
+        dataSource = DefaultCharactersRemoteDataSource(networkManager: networkManager)
+         let expectation = expectation(description: "Test Remote API up and running")
+
+        networkManager.fetchAllCharacters(withParameters: [:])
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    XCTFail("Request failed with error: \(error)")
+                }
+            } receiveValue: { data, response in
+                if let httpResponse = response as? HTTPURLResponse {
+                    XCTAssertEqual(httpResponse.statusCode, 200, "Expected HTTP status code 200")
+                    expectation.fulfill()
+                } else {
+                    XCTFail("invalid response received")
+                }
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 10)
     }
 }
